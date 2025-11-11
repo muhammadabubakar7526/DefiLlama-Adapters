6e4f1b475cfe1080f219fad885dd77b8a091daa4
muhammad abubakar <81152955+muhammadabubakar7526@users.noreply.github.com>
2025-11-11 16:33:16 +0500

    want to update dex name and calculate correct tvl

--- projects/kfc.js
+++ projects/kfc.js
@@ -1,7 +1,7 @@
 const { uniV3Export } = require("./helper/uniswapV3")
 
 const factory = '0x09df701f1f5df83a3bbef7da4e74bb075199d6a4' // same on all chains
-
+// This PR is created for correct TVL and update dex Name to Kaspa Finance V3 (KFC)
 module.exports = uniV3Export({
   kasplex: { factory, fromBlock: 484634, }
 })
